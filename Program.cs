b720720fbe389d0bb8b93620441af76af2d4c1ce
GabrielDebug <you@example.com>
2026-02-18 19:03:06 -0300

    create Criar

--- Program.cs
+++ Program.cs
@@ -6,26 +6,55 @@ namespace TextEditor
     {
         static void Main()
         {
-            
+            Menu();
         }
 
         static void Menu()
         {
             Console.Clear();
-            Console.WriteLie("Oq você deseja fazer? ");
-            Console.WriteLie("1 - Abrir um arquivo ");
-            Console.WriteLie("2 - Criar um novo arquivo ");
-            Console.WriteLie("3 - Sair ");
+            Console.WriteLine("Oq você deseja fazer? ");
+            Console.WriteLine("1 - Abrir um arquivo ");
+            Console.WriteLine("2 - Criar um novo arquivo ");
+            Console.WriteLine("3 - Sair ");
 
             short option = short.Parse(Console.ReadLine());
 
             switch (option)
             {
-                case 1:
+                case 0: System.Environment.Exit(0);
+                    break;
+                case 1: Abrir();
+                    break;
+                case 2: Criar();
+                    break;
+                default: Menu();
+                    break;
+
             }
         }
-        
-        
+
+        static void Abrir()
+        {
+            
+        }
+
+        static void Criar()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite o Seu texto abaixo (Digite ESC para sair)");
+            Console.WriteLine("___________________");
+
+            string text = "";
+
+            do
+            {
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Console.WriteLine(text);
+        }
     }
 }
 
