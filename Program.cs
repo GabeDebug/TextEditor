c77c30420756097cf0bb9541280853df3f230e64
GabrielDebug <you@example.com>
2026-02-18 18:32:10 -0300

    Create Menu

--- Program.cs
+++ Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using System;
+
+namespace TextEditor
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLie("Oq você deseja fazer? ");
+            Console.WriteLie("1 - Abrir um arquivo ");
+            Console.WriteLie("2 - Criar um novo arquivo ");
+            Console.WriteLie("3 - Sair ");
+
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+            }
+        }
+        
+        
+    }
+}
+
